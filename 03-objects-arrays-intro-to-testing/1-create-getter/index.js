162dd6840799137fc450076cbbc65b68bece6def
Glance1990 <paulkadysh@gmail.com>
2022-05-03 22:37:42 +0300

    Homework-3

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,11 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const stringArr = path.split(".");
 
+  return function (object) {
+    return stringArr.reduce((acc, item) => {
+      return acc ? acc[item] : undefined;
+    }, object);
+  };
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,27 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (!string.length || size === 0) {
+    return "";
+  }
+  if (size === undefined) {
+    return string;
+  }
 
+  let letter = "";
+  let counter = 0;
+  let resultArr = [];
+
+  string.split("").forEach((item) => {
+    if (letter !== item) {
+      letter = item;
+      counter = 1;
+      resultArr.push(item);
+    } else if (counter < size) {
+      counter++;
+      resultArr.push(item);
+    }
+  });
+
+  return resultArr.join("");
 }
